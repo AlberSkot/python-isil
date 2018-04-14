a306e6e2477f4d884dfea513ce4306df5e2d9c43
AlberSkot <38021633+AlberSkot@users.noreply.github.com>
2018-04-14 16:49:02 -0500

    Add files via upload

---
+++ sesion4/Ejercicio1Foro.py
@@ -0,0 +1,17 @@
+producto= ('Lavadora')
+precio= int(323)
+
+conversion=precio/3.23
+
+print('Producto: ',producto)
+print('El precio en soles es: ',precio,'Soles')
+print('El precio en dolares es: ',conversion,'Dolares')
+
+producto= ('Reproductor')
+precio= int(1200)
+
+conversion=precio/3.23
+
+print('Producto: ',producto)
+print('El precio en soles es: ',precio,'Soles')
+print('El precio en dolares es: ',conversion,'Dolares')
